3e1caff2ceb02e07bcd18767a8b63a7929b12dff
amitrajput786 <amitrajput51169@gmail.com>
2026-01-13 00:14:31 +0530

    after update

--- Resnet/problem-113.py
+++ Resnet/problem-113.py
@@ -5,4 +5,18 @@ Created on Mon Jan 12 22:48:23 2026
 
 @author: amit
 """
-
+import numpy as np 
+def residual_block(x,w1,w2,):
+    x=np.array(x)
+    w1=np.array(w1)
+    w2=np.array(w2)
+    l=w1@(x)
+    z=np.maximum(0,l)
+    l2=w2@(z)
+    a=l2+x
+    z=np.maximum(0,a)
+    return z
+x=[[1,1],[2,2]]
+w1=[[1,0],[0,2]]
+w2=[[0,1],[3,0]]
+print(residual_block(x,w1,w2))
